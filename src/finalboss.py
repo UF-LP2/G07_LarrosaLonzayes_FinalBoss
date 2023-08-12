5946a0e926248f99cf31c1e701dbac2c4e0e8bda
MatiasLarrosa <matularrosa555@gmail.com>
2023-08-12 15:15:11 -0300

    clases agregadas

--- src/finalboss.py
+++ src/finalboss.py
@@ -1,1 +1,48 @@
 # Desarrollo del trabajo
+
+# Se crea un nuevo objeto de barco basado en las observaciones de la tripulacion
+# Barco1 = Ship(15, 10)
+
+# Si ven el ingreso de un cargo al puerto, crearán un nuevo objeto de cargo
+# Maersk = Cargo(15, 10)
+
+# Si ven el ingreso de un crucero al puerto, crearán un nuevo objeto de crucero
+# Titanic = Cruise(34, 15, 10)
+
+# Tienes acceso a los atributos "draft" (borrador) y "crew" (tripulación) del barco. 
+# "Draft" == peso total del barco
+# "Crew" == número de personas trabajando en el barco.
+# Cada miembro de la tripulación agrega 1.5 unidades al borrador
+# El cargo agrega 3.5 si la calidad es 1, agrega 2 si la calidad es 0.5 y agrega 0.5 si la calidad es 0.25
+# Un pasajero agrega 2.25 (exclusivo de cruceros)
+# Luego de restar el peso de tripulacion (y de pasajeros en los cruceros), si sigue siendo >=20, hay que saquear
+
+# Agrega este metodo, para decidir si el barco merece ser saqueado:
+
+# is_worth_it(...)
+
+# Las clases Cargo y Cruise son hijas de Ship
+class cShip:
+	def __init__(self, draft, crew):
+		self.draft = draft
+		self.crew = crew
+
+# Esta es la sintaxis para desarrollar una clase hija
+class cCargo(cShip):
+	def __init__(self, cargo, quality, draft, crew):
+        # Usando super(), no es necesario usar el nombre del elemento padre, 
+	    # sino que automaticamente heredará los metodos y propiedades de la clase padre
+		super().__init__(draft, crew)
+		
+		self.cargo = cargo
+		self.quality = quality
+		# Completar herencia
+        
+
+class cCruise(cShip):
+	def __init__(self, passengers, draft, crew):
+		# Idem que en cCargo para usar super()
+		super().__init__(draft, crew)
+		
+		self.passengers = passengers
+		# Completar herencia
