a9aaf4401917d931dc8fae17cef8821d6f3377a2
MatiasLarrosa <matularrosa555@gmail.com>
2023-08-15 14:40:12 -0300

    comentamos cosas

--- main.py
+++ main.py
@@ -57,14 +57,14 @@ def main() -> None:
 
 	# Al ser listaBarcos como un listado de C++,
 	# hay que usar len() para saber la cantidad de items que posee, tal que 
-	for i in range(len(listaBarcos)):
-		print("Numero %d" % (i+1))
-		try:
-			auxBarco = listaBarcos[i]
-			botin = float(auxBarco.is_worth_it())
-			print("S A Q U E A B L E, " , "Botin = %.2f" % botin)
-		except Exception as e:
-			print(str(e))
+	# for i in range(len(listaBarcos)):
+	# 	print("Numero %d" % (i+1))
+	# 	try:
+	# 		auxBarco = listaBarcos[i]
+	# 		botin = float(auxBarco.is_worth_it())
+	# 		print("S A Q U E A B L E, " , "Botin = %.2f" % botin)
+	# 	except Exception as e:
+	# 		print(str(e))
 
 
 if __name__ == "__main__":
